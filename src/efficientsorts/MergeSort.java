cf85d6fccffa5cf986abbcc9e1a7d4f276c93e17
NUNES <nunesfariasgustavo@gmail.com>
2026-06-29 20:12:11 -0300

    feat: creat merge sort class

---
+++ src/efficientsorts/MergeSort.java
@@ -0,0 +1,12 @@
+package efficientsorts;
+
+import interfaces.ISort;
+
+public class MergeSort<T extends Comparable<T>> implements ISort<T> {
+
+
+    @Override
+    public void sort(T[] array) {
+
+    }
+}
