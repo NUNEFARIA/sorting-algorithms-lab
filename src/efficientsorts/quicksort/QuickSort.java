dd7113c1f581fa1fbbda31a9f3d4f504247c6ae7
NUNES <nunesfariasgustavo@gmail.com>
2026-06-30 15:51:29 -0300

    feat: creat quicksort class

---
+++ src/efficientsorts/quicksort/QuickSort.java
@@ -0,0 +1,12 @@
+package efficientsorts.quicksort;
+
+import interfaces.ISort;
+
+public class QuickSort<T extends Comparable<T>> implements ISort<T> {
+
+
+    @Override
+    public void sort(T[] array) {
+
+    }
+}
