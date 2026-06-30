1ee2d027225d695d8541ae7630d2f89cf1865b98
NUNES <nunesfariasgustavo@gmail.com>
2026-06-30 19:28:56 -0300

    feat: add quicksort method

--- src/efficientsorts/quicksort/QuickSort.java
+++ src/efficientsorts/quicksort/QuickSort.java
@@ -8,5 +8,27 @@ public class QuickSort<T extends Comparable<T>> implements ISort<T> {
     @Override
     public void sort(T[] array) {
 
+        if (array == null || array.length <= 1) {
+            return;
+        }
+
+        quickSort(array, 0, array.length - 1);
+
     }
+
+    private void quickSort(T[] array, int left, int right) {
+
+        if (left < right) {
+            int pivot = partition(array, left, right);
+            quickSort(array, left, pivot - 1);
+            quickSort(array, pivot + 1, right);
+        }
+
+    }
+
+    private int partition(T[] array, int left, int right) {
+
+        return
+    }
+
 }
